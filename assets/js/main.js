796b19dbc71493a6a064194864d9f6d4cda079aa
truss-de <122530180+truss-de@users.noreply.github.com>
2023-01-15 17:49:36 +0100

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -53,7 +53,8 @@
 
 		}
 
-	// Nav.
+	// Nav main.
+	
 		var $nav = $header.children('nav'),
 			$nav_li = $nav.find('li');
 
@@ -65,6 +66,49 @@
 
 			}
 
+	// Dropdowns.
+	$('#nav > ul').dropotron({
+		mode: 'fade',
+		noOpenerFade: true,
+		alignment: 'center',
+		detach: false
+	});
+
+// Nav.
+
+	// Title Bar.
+		$(
+			'<div id="titleBar">' +
+				'<a href="#navPanel" class="toggle"></a>' +
+				'<span class="title">' + $('#logo h1').html() + '</span>' +
+			'</div>'
+		)
+			.appendTo($body);
+
+	// Panel.
+		$(
+			'<div id="navPanel">' +
+				'<nav>' +
+					$('#nav').navList() +
+				'</nav>' +
+			'</div>'
+		)
+			.appendTo($body)
+			.panel({
+				delay: 500,
+				hideOnClick: true,
+				hideOnSwipe: true,
+				resetScroll: true,
+				resetForms: true,
+				side: 'left',
+				target: $body,
+				visibleClass: 'navPanel-visible'
+			});
+
+
+
+
+			
 	// Main.
 		var	delay = 325,
 			locked = false;
